7fd7e33bc404247c8c449035c2eb5f3d07c2bb4b
Goruchie <marcosgualtero43253@gmail.com>
2025-04-28 14:00:49 -0300

    Create the header component and basic styling

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="header"> {{ title() }} </div>
+  `,
+  styles: `
+  .header {
+    background: rgba(0, 0, 0, .5);
+    color: black;
+    padding: 1rem;
+  }
+  `
+})
+export class HeaderComponent {
+  title = signal('Angular Ecommerce');
+}
