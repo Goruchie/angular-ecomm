e1105237f5f20192b2cbd4bd41448e8d1cb8717e
Goruchie <marcosgualtero43253@gmail.com>
2025-04-28 23:13:05 -0300

    Install Tailwind and add a 'Cart' button for the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="header"> {{ title() }} </div>
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
+      <span class="text-xl">
+        My Store
+      </span>
+      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+    </div>
   `,
-  styles: `
-  .header {
-    background: rgba(0, 0, 0, .5);
-    color: black;
-    padding: 1rem;
-  }
-  `
+  styles: ``
 })
 export class HeaderComponent {
-  title = signal('Angular Ecommerce');
+
+  showButtonClicked() {
+    console.log("btn checked!");
+  }
 }
