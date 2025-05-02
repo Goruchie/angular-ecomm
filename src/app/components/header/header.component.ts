c8442129c2204efc41aa20f5e00dccf1c91939ee
Goruchie <marcosgualtero43253@gmail.com>
2025-05-01 23:40:57 -0300

    Implement the componens with its routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
+import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, RouterLink],
   template: `
     <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
-      <span class="text-xl">
+      <button class="text-xl cursor-pointer" routerLink="/">
         My Store
-      </span>
-      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+      </button>
+      <app-primary-button 
+      [label]=" 'Cart ('+ cartService.cart().length + ')'"
+      routerLink="/cart"
+      />
     </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
 
-  showButtonClicked() {
-    console.log("btn checked!");
-  }
+  cartService = inject(CartService);
 }
