ff7d51d7d97d72eaba4aae8cca04db8c9e6f1c96
Goruchie <marcosgualtero43253@gmail.com>
2025-05-02 00:37:51 -0300

    Build the cart page and implement the remove from cart feature

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,10 +6,15 @@ import { Product } from '../models/products.model';
 })
 export class CartService {
 
-  cart = signal<Product[]>([]);
+  cart = signal<Product[]>([
+  ]);
 
   addToCart(product: Product) {
     this.cart.set([...this.cart(), product]);
+  }
+
+  removeFromCart(id: number) {
+    this.cart.set(this.cart().filter((p) => p.id !== id));
   }
 
   constructor() { }
